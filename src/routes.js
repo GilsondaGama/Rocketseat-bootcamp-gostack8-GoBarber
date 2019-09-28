3fcfae6dbd4f0cfbf33fe0059a305b55438f2fbe
Gilson da Gama <gilsondagama@hotmail.com>
2019-09-28 15:12:39 -0300

    Marcar Notifição como lida

--- src/routes.js
+++ src/routes.js
@@ -30,6 +30,7 @@ routes.post('/Appointments', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
