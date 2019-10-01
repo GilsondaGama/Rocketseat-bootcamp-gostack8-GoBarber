0724d9839da6e48687f19b18681ca4fe3f0489f6
Gilson da Gama <gilsondagama@hotmail.com>
2019-10-01 04:37:55 -0300

    Configurando Nodemailer, Template de Email e envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '06dba7af98d559',
+    pass: '1e1b1a0b6ad330',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarbar.com',
+  },
+};
